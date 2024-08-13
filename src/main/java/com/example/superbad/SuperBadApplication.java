2e68973c48abe4c4b20ec6004cc38415ba0372b0
O-damnpr <ni1722650@gmail.com>
2024-08-13 15:16:26 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/superbad/SuperBadApplication.java
@@ -0,0 +1,13 @@
+package com.example.superbad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperBadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperBadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/superbad/SuperBadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superbad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperBadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
